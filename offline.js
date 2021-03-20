03e04738bbe9a2b9fc9b47c5e4e0322fa057d376
narmundolegal <80333793+narmundolegal@users.noreply.github.com>
2021-03-20 11:08:29 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1616249245,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/tiro-sheet0.png",
+		"images/dinossauro-sheet0.png",
+		"images/dinossauro-sheet1.png",
+		"images/dinossauro-sheet2.png",
+		"images/tilemap.png",
+		"images/vida.png",
+		"images/spritefont.png",
+		"images/tela_morte-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
